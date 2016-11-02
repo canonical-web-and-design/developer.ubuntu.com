2d4ae154ffbb05ce6b428ece5bcc687521a94e11
Will Moggridge <will@moggridge.com>
2016-11-02 12:48:36 +0000

    Add markdown view to load template and metadata

---
+++ templates/pages/core/index.md
@@ -0,0 +1,5 @@
+# Core
+
+Hello!
+
+[Example topic](/core/reference/example-topic)
--- webapp/urls.py
+++ webapp/urls.py
@@ -4,12 +4,15 @@ from django_yaml_redirects import load_redirects
 from django_template_finder_view import TemplateFinder
 
 # Local
-from webapp.views import custom_404, custom_500
+from webapp.views import custom_404, custom_500, MarkdownView
 
 # Match any redirects first
 urlpatterns = load_redirects()
 
 # Try to find templates
+urlpatterns.append(
+    url(r'^(?P<path>core(/.*)?)$', MarkdownView.as_view())
+)
 urlpatterns.append(url(r'^(?P<template>.*)/?$', TemplateFinder.as_view()))
 
 # Error handlers
