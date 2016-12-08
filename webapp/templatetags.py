8652d3277d6e99865240ae1c3be355a810ae664f
Will Moggridge <will@moggridge.com>
2016-12-08 13:50:52 +0000

    Add insights feed block for modular page

--- webapp/templatetags.py
+++ webapp/templatetags.py
@@ -1,8 +1,28 @@
 from django import template
 
+from webapp.lib.feeds import get_json_feed_content, get_rss_feed_content
 from webapp.lib.markdown import get_page_data
 
 register = template.Library()
+
+
+@register.simple_tag
+def get_json_feed(feed_url, **kwargs):
+    return get_json_feed_content(feed_url, **kwargs)
+
+
+@register.simple_tag
+def get_rss_feed(feed_url, **kwargs):
+    return get_rss_feed_content(feed_url, **kwargs)
+
+
+@register.inclusion_tag(
+    'includes/components/feed_cards.html'
+)
+def feed_cards(feed_url, **kwargs):
+    context = kwargs
+    context['feed_url'] = feed_url
+    return context
 
 
 @register.inclusion_tag(
