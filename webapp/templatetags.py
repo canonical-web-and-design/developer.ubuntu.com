44d43f7cd770e14d9dca6d48799fb826c428258f
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-03 20:55:31 +0100

    Provide fallbacks on error for feeds

--- webapp/templatetags.py
+++ webapp/templatetags.py
@@ -1,7 +1,7 @@
 import dateutil.parser
 from django import template
 from operator import itemgetter
-
+from canonicalwebteam.get_feeds import get_json_feed_content
 from webapp.lib.markdown import get_page_data as _get_page_data
 from webapp.sitemap import sitemap
 from django.utils.html import conditional_escape
@@ -70,7 +70,12 @@ def tutorial_cards(feed_config, limit=3):
         else:
             feed_type = '-'.join(['recent', category])
         feed_url = base_feed_url.format(type=feed_type)
-        feed_data += get_json_feed_content(feed_url, limit=limit)
+
+        category_feed = get_json_feed_content(feed_url, limit=limit)
+        if category_feed:
+            feed_data += category_feed
+        else:
+            return {'feed': False}
 
     # Sort tutorials by published, descending. Grab limit from aggregated items
     feed_data = sorted(feed_data, key=itemgetter('published'), reverse=True)
